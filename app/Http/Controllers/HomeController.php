bacdfc03d0e62a33b4d846515f087a892ce0db94
dzaki-abd <muhammad.dzaki@mhs.itenas.ac.id>
2024-09-25 01:51:48 +0700

    update dashboard done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Merchant;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $value = Transaction::all();
+
+        $value = $value->unique('invoice_number');
+
+        if (auth()->user()->hasRole('merchant')) {
+            $merchant = Merchant::where('user_id', auth()->id())->first();
+            $value = Transaction::where('merchant_id', $merchant->id)->get();
+        } else if (auth()->user()->hasRole('customer')) {
+            $value = Transaction::where('user_id', auth()->id())->get();
+        }
+
+        // hitung total transaksi
+        $total = $value->sum('total');
+        return view('dashboard', compact('total'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,13 +12,13 @@
     <div class="row">
 
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        {{-- <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Earnings (Monthly)</div>
+                                Earnings</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
                         </div>
                         <div class="col-auto">
@@ -27,7 +27,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
@@ -36,8 +36,14 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                @if (auth()->user()->hasRole('customer'))
+                                    Expending
+                                @else
+                                    Earnings
+                                @endif
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp {{ number_format($total, 0, ',', '.') }}
+                            </div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
