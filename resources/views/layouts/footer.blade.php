c4644595d21e59e7f045f799f3e2d3397609e19e
dzaki-abd <muhammad.dzaki@mhs.itenas.ac.id>
2024-09-24 16:19:36 +0700

    slicing main temp

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <a href="https://github.com/dzaki-abd">Dzaki-abd</a>
+                <script type="text/javascript">
+                    document.write(new Date().getFullYear());
+                </script>
+            </span>
+        </div>
+    </div>
+</footer>
